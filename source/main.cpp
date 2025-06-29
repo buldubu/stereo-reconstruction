cd667292f6a16967059dc94703a489e9b72e747b
muhammedbyrm <muhammedbm88@gmail.com>
2025-06-29 20:41:13 +0200

    Implement disparity map computation and 3D point cloud generation

--- source/main.cpp
+++ source/main.cpp
@@ -4,6 +4,7 @@
 #include <opencv2/features2d.hpp>
 #include <opencv2/calib3d.hpp>
 #include <opencv2/imgproc.hpp>
+#include <fstream>
 
 const bool USE_GRAYSCALE = false;
 
@@ -18,16 +19,28 @@ int main(int argc, const char* argv[])
         cv::cvtColor(imgL, imgL, cv::COLOR_BGR2GRAY);
         cv::cvtColor(imgR, imgR, cv::COLOR_BGR2GRAY);
     }
+   
+    double fx = 3997.684;
+    double cxL = 1176.728;
+    double cxR = 1307.839;
+    double cy = 1011.728;
+    double baseline = 193.001; 
+
+    // Q matrix based on calib.txt
+    cv::Mat Q = (cv::Mat_<double>(4,4) << 
+        1, 0, 0, -cxL,
+        0, 1, 0, -cy,
+        0, 0, 0, fx,
+        0, 0, 1.0 / baseline, (cxR - cxL) / baseline);   //  0, 0, -1.0 / baseline, (cxL - cxR) / baseline)
 
     // hard-coded from the calib file
-    cv::Mat K1 = (cv::Mat_<double>(3,3) << 3997.684, 0, 1176.728,
-                                       0, 3997.684, 1011.728,
+    cv::Mat K1 = (cv::Mat_<double>(3,3) << fx, 0, cxL,
+                                       0, fx, cy,
                                        0, 0, 1);
-    cv::Mat K2 = (cv::Mat_<double>(3,3) << 3997.684, 0, 1307.839,
-                                       0, 3997.684, 1011.728,
+    cv::Mat K2 = (cv::Mat_<double>(3,3) << fx, 0, cxR,
+                                       0, fx, cy,
                                        0, 0, 1);
-    const double baseline = 193.001;
-
+   
     // detect & match features on the original images
     cv::Ptr<cv::SIFT> siftRaw = cv::SIFT::create();
     std::vector<cv::KeyPoint> kptsL_raw, kptsR_raw;
@@ -56,10 +69,11 @@ int main(int argc, const char* argv[])
     cv::Mat maskF;
     cv::Mat F = cv::findFundamentalMat(ptsL, ptsR, cv::FM_RANSAC, 3.0, 0.99, maskF);
 
+    /*
     // essential matrix and pose recovery with known intrinsics
     cv::Mat E = K2.t() * F * K1;
     cv::Mat R, T;
-    //   recover the actual rotation and translation
+    // recover the actual rotation and translation
     cv::recoverPose(E, ptsL, ptsR, K1, R, T, maskF);
 
     //  assume zero distortion
@@ -67,17 +81,22 @@ int main(int argc, const char* argv[])
     cv::Mat R1, R2, P1, P2, Q;
     cv::Size imgSize = imgL.size();
     cv::stereoRectify(K1, distCoeffs, K2, distCoeffs, imgSize, R, T,
-                      R1, R2, P1, P2, Q, cv::CALIB_ZERO_DISPARITY, 0, imgSize);
+                      R1, R2, P1, P2, Q, cv::CALIB_ZERO_DISPARITY, 0, imgSize);  */
 
-    // build rectification maps
+    /*// build rectification maps
     cv::Mat map1x, map1y, map2x, map2y;
     cv::initUndistortRectifyMap(K1, distCoeffs, R1, P1, imgSize, CV_32FC1, map1x, map1y);
-    cv::initUndistortRectifyMap(K2, distCoeffs, R2, P2, imgSize, CV_32FC1, map2x, map2y);
+    cv::initUndistortRectifyMap(K2, distCoeffs, R2, P2, imgSize, CV_32FC1, map2x, map2y); 
 
     // remap the pixels
     cv::Mat imgLRect, imgRRect;
     cv::remap(imgL, imgLRect, map1x, map1y, cv::INTER_LINEAR);
-    cv::remap(imgR, imgRRect, map2x, map2y, cv::INTER_LINEAR);
+    cv::remap(imgR, imgRRect, map2x, map2y, cv::INTER_LINEAR);  */
+
+    cv::Mat imgLRect, imgRRect;
+
+    imgLRect = imgL.clone();
+    imgRRect = imgR.clone(); 
 
     cv::imwrite(outputName + "rectified_left.jpg", imgLRect);
     cv::imwrite(outputName + "rectified_right.jpg", imgRRect);
@@ -125,6 +144,86 @@ int main(int argc, const char* argv[])
     // cv::waitKey(0);
     // cv::destroyAllWindows();
     cv::imwrite(outputName + "sift_good_matches.jpg", imgMatches);
+
+    // disparity map calculation
+    cv::Mat imgLGray, imgRGray;
+    if (USE_GRAYSCALE) {
+        imgLGray = imgLRect;
+        imgRGray = imgRRect;
+    } else {
+        cv::cvtColor(imgLRect, imgLGray, cv::COLOR_BGR2GRAY);
+        cv::cvtColor(imgRRect, imgRGray, cv::COLOR_BGR2GRAY);
+    }
+
+    int blockSize = 9;
+    int numDisparities = 288;
+    cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create(0, numDisparities, blockSize);
+
+    sgbm->setBlockSize(blockSize);
+    // sgbm->setMinDisparity(0);
+    sgbm->setMinDisparity(-32);
+    sgbm->setNumDisparities(numDisparities);
+    sgbm->setP1(8 * imgLGray.channels() * blockSize * blockSize);
+    sgbm->setP2(32 * imgLGray.channels() * blockSize * blockSize);
+    sgbm->setMode(cv::StereoSGBM::MODE_SGBM_3WAY);
+
+    cv::Mat disp;
+    sgbm->compute(imgLGray, imgRGray, disp);
+
+    cv::Mat dispFloat;
+    disp.convertTo(dispFloat, CV_32F, 1.0 / 16.0);
+
+    cv::Mat dispVis;
+    cv::normalize(dispFloat, dispVis, 0, 255, cv::NORM_MINMAX, CV_8U);
+    cv::imwrite(outputName + "disparity_map.jpg", dispVis);
+
+    cv::Mat dispColor;
+    cv::applyColorMap(dispVis, dispColor, cv::COLORMAP_JET);
+    cv::imwrite(outputName + "colored_disp.jpg", dispColor);
+
+    cv::Mat depthMap;
+    cv::reprojectImageTo3D(dispFloat, depthMap, Q);
+
+    std::vector<cv::Mat> xyz;
+    cv::split(depthMap, xyz); // xyz[2] = z (depth)
+
+    // Write to PLY 
+    std::ofstream out(outputName +"pointcloud.ply");
+    out << "ply\nformat ascii 1.0\n";
+
+    // Count valid points
+    int validPoints = 0;
+    for (int y = 0; y < depthMap.rows; ++y) {
+        for (int x = 0; x < depthMap.cols; ++x) {
+            float d = dispFloat.at<float>(y, x);
+            if (d > 0)
+                ++validPoints;
+        }
+    }
+
+    // write header
+    out << "element vertex " << validPoints << "\n";
+    out << "property float x\nproperty float y\nproperty float z\n";
+    out << "property uchar red\nproperty uchar green\nproperty uchar blue\n"; // uncomment for color
+    out << "end_header\n";
+
+    // write points 
+    for (int y = 0; y < depthMap.rows; ++y) {
+        for (int x = 0; x < depthMap.cols; ++x) {
+            float d = dispFloat.at<float>(y, x);
+            if (d <= 0)
+                continue;
+
+            cv::Vec3f point = depthMap.at<cv::Vec3f>(y, x);
+            
+            cv::Vec3b color = imgLRect.at<cv::Vec3b>(y, x);
+            out << point[0] << " " << point[1] << " " << point[2] << " "
+            << (int)color[2] << " " << (int)color[1] << " " << (int)color[0] << "\n";
+        }
+    }
+
+    out.close();
+    
     
     return 0;
 }
