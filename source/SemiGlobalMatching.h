9084d64f28c3cd73077783889ee09f546fa71627
Muhammed Bayram <70086650+muhammedbyrm@users.noreply.github.com>
2025-07-13 12:00:40 +0200

    Update SemiGlobalMatching.h

--- source/SemiGlobalMatching.h
+++ source/SemiGlobalMatching.h
@@ -1,46 +1,42 @@
-#pragma once
+#ifndef SEMI_GLOBAL_MATCHING_H
+#define SEMI_GLOBAL_MATCHING_H
 
 #include <opencv2/opencv.hpp>
 #include <vector>
-#include <cstdint>
+#include <string>
 
 class SemiGlobalMatching {
 public:
-    SemiGlobalMatching(const cv::Mat& left,
-                       const cv::Mat& right,
-                       int max_disp = 64,
-                       int census_size = 9,
-                       int filter_size = 5,
-                       int occlusion_seuil = 1,
-                       int penalty_1 = 10,
-                       int penalty_2 = 120);
+    SemiGlobalMatching(
+        const cv::Mat& left,
+        const cv::Mat& right,
+        int max_disp,
+        int census_size,
+        int filter_size,
+        int occlusion_seuil,
+        int penalty_1,
+        int penalty_2
+    );
 
     void SGM_process();
-
-    cv::Mat get_disparity() const;
-
-    // Raw float disparity (0 - max_disp)
-    cv::Mat get_disparity_float() const;
+    cv::Mat get_disparity() const;       
+    cv::Mat get_disparity_float() const;  
 
 private:
-    int max_disp;
-    int census_size;
-    int filter_size;
-    int occlusion_seuil;
-    int penalty_1;
-    int penalty_2;
+    int max_disp, census_size, filter_size;
+    int occlusion_seuil, penalty_1, penalty_2;
+    int height, width;
 
-    cv::Mat left_I;
-    cv::Mat right_I;
-    int height;
-    int width;
+    cv::Mat left_I, right_I;
+    cv::Mat disparities;      
+    cv::Mat disparity_f32;    
 
     std::vector<cv::Mat> left_cost_volume;
     std::vector<std::vector<cv::Mat>> aggregation_volume;
 
-    cv::Mat disparities;
-    cv::Mat disparity_f32;
-
     void compute_costs();
     void aggregate_costs();
+    cv::Mat get_path_cost(const cv::Mat& block);
 };
+
+#endif 
