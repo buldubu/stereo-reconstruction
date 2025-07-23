04dc66a80287680421af91d8137ca438dac82f3e
Muhammed Bayram <muhammedbm88@gmail.com>
2025-07-23 21:13:38 +0200

    Update params in main.cpp and sgm.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, const char* argv[])
     // Load data
     cv::Mat imgL = cv::imread("../Data/Motorcycle-perfect/im0.png", cv::IMREAD_COLOR);
     cv::Mat imgR = cv::imread("../Data/Motorcycle-perfect/im1.png", cv::IMREAD_COLOR);
-    
+
     if (USE_GRAYSCALE) {
         cv::cvtColor(imgL, imgL, cv::COLOR_BGR2GRAY);
         cv::cvtColor(imgR, imgR, cv::COLOR_BGR2GRAY);
@@ -30,23 +30,23 @@ int main(int argc, const char* argv[])
     double cxL = 1176.728 * scale;
     double cxR = 1307.839 * scale;
     double cy = 1011.728 * scale;
-    double baseline = 193.001; 
+    double baseline = 193.001;
 
     // Q matrix based on calib.txt
-    cv::Mat Q = (cv::Mat_<double>(4,4) << 
+    cv::Mat Q = (cv::Mat_<double>(4, 4) <<
         1, 0, 0, -cxL,
         0, 1, 0, -cy,
         0, 0, 0, fx,
         0, 0, 1.0 / baseline, (cxR - cxL) / baseline);   //  0, 0, -1.0 / baseline, (cxL - cxR) / baseline)
 
     // hard-coded from the calib file
-    cv::Mat K1 = (cv::Mat_<double>(3,3) << fx, 0, cxL,
-                                       0, fx, cy,
-                                       0, 0, 1);
-    cv::Mat K2 = (cv::Mat_<double>(3,3) << fx, 0, cxR,
-                                       0, fx, cy,
-                                       0, 0, 1);
-   
+    cv::Mat K1 = (cv::Mat_<double>(3, 3) << fx, 0, cxL,
+        0, fx, cy,
+        0, 0, 1);
+    cv::Mat K2 = (cv::Mat_<double>(3, 3) << fx, 0, cxR,
+        0, fx, cy,
+        0, 0, 1);
+
     // detect & match features on the original images
     cv::Ptr<cv::SIFT> siftRaw = cv::SIFT::create();
     std::vector<cv::KeyPoint> kptsL_raw, kptsR_raw;
@@ -89,29 +89,29 @@ int main(int argc, const char* argv[])
     cv::stereoRectify(K1, distCoeffs, K2, distCoeffs, imgSize, R, T,
                       R1, R2, P1, P2, Q, cv::CALIB_ZERO_DISPARITY, 0, imgSize);  */
 
-    /*// build rectification maps
-    cv::Mat map1x, map1y, map2x, map2y;
-    cv::initUndistortRectifyMap(K1, distCoeffs, R1, P1, imgSize, CV_32FC1, map1x, map1y);
-    cv::initUndistortRectifyMap(K2, distCoeffs, R2, P2, imgSize, CV_32FC1, map2x, map2y); 
+                      /*// build rectification maps
+                      cv::Mat map1x, map1y, map2x, map2y;
+                      cv::initUndistortRectifyMap(K1, distCoeffs, R1, P1, imgSize, CV_32FC1, map1x, map1y);
+                      cv::initUndistortRectifyMap(K2, distCoeffs, R2, P2, imgSize, CV_32FC1, map2x, map2y);
 
-    // remap the pixels
-    cv::Mat imgLRect, imgRRect;
-    cv::remap(imgL, imgLRect, map1x, map1y, cv::INTER_LINEAR);
-    cv::remap(imgR, imgRRect, map2x, map2y, cv::INTER_LINEAR);  */
+                      // remap the pixels
+                      cv::Mat imgLRect, imgRRect;
+                      cv::remap(imgL, imgLRect, map1x, map1y, cv::INTER_LINEAR);
+                      cv::remap(imgR, imgRRect, map2x, map2y, cv::INTER_LINEAR);  */
 
     cv::Mat imgLRect, imgRRect;
 
     imgLRect = imgL.clone();
-    imgRRect = imgR.clone(); 
+    imgRRect = imgR.clone();
 
     cv::imwrite(outputName + "rectified_left.jpg", imgLRect);
     cv::imwrite(outputName + "rectified_right.jpg", imgRRect);
 
     // detect key-points & descriptors, basically as before but on the rectified images
-    cv::Ptr<cv::SIFT> sift = cv::SIFT::create(); 
+    cv::Ptr<cv::SIFT> sift = cv::SIFT::create();
     std::vector<cv::KeyPoint> kptsL, kptsR;
     cv::Mat descL, descR;
-    sift->detectAndCompute(imgLRect,  cv::noArray(), kptsL, descL);
+    sift->detectAndCompute(imgLRect, cv::noArray(), kptsL, descL);
     sift->detectAndCompute(imgRRect, cv::noArray(), kptsR, descR);
 
     // draw SIFT keypoints on the images
@@ -140,10 +140,10 @@ int main(int argc, const char* argv[])
     // draw matches
     cv::Mat imgMatches;
     cv::drawMatches(imgLRect, kptsL, imgRRect, kptsR,
-                    goodMatches, imgMatches,
-                    cv::Scalar::all(-1), cv::Scalar::all(-1),
-                    std::vector<char>(),
-                    cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+        goodMatches, imgMatches,
+        cv::Scalar::all(-1), cv::Scalar::all(-1),
+        std::vector<char>(),
+        cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
 
     // save keypoints matches
     // cv::imshow("Good SIFT Matches (Left vs Right)", imgMatches);
@@ -156,18 +156,19 @@ int main(int argc, const char* argv[])
     if (USE_GRAYSCALE) {
         imgLGray = imgLRect;
         imgRGray = imgRRect;
-    } else {
+    }
+    else {
         cv::cvtColor(imgLRect, imgLGray, cv::COLOR_BGR2GRAY);
         cv::cvtColor(imgRRect, imgRGray, cv::COLOR_BGR2GRAY);
     }
 
-    int blockSize = 9;
-    int numDisparities = 192;
+    int blockSize = 5;
+    int numDisparities = 128;
     cv::Ptr<cv::StereoSGBM> sgbm = cv::StereoSGBM::create(0, numDisparities, blockSize);
 
     sgbm->setBlockSize(blockSize);
     // sgbm->setMinDisparity(0);
-    sgbm->setMinDisparity(-32);
+    sgbm->setMinDisparity(0);
     sgbm->setNumDisparities(numDisparities);
     sgbm->setP1(8 * imgLGray.channels() * blockSize * blockSize);
     sgbm->setP2(32 * imgLGray.channels() * blockSize * blockSize);
@@ -194,7 +195,7 @@ int main(int argc, const char* argv[])
     cv::split(depthMap, xyz); // xyz[2] = z (depth)
 
     // Write to PLY 
-    std::ofstream out(outputName +"pointcloud.ply");
+    std::ofstream out(outputName + "pointcloud.ply");
     out << "ply\nformat ascii 1.0\n";
 
     // Count valid points
@@ -221,10 +222,10 @@ int main(int argc, const char* argv[])
                 continue;
 
             cv::Vec3f point = depthMap.at<cv::Vec3f>(y, x);
-            
+
             cv::Vec3b color = imgLRect.at<cv::Vec3b>(y, x);
             out << point[0] << " " << point[1] << " " << point[2] << " "
-            << (int)color[2] << " " << (int)color[1] << " " << (int)color[0] << "\n";
+                << (int)color[2] << " " << (int)color[1] << " " << (int)color[0] << "\n";
         }
     }
 
@@ -240,7 +241,7 @@ int main(int argc, const char* argv[])
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
             float d = dispFloat.at<float>(y, x);
-            if (d <= 0) 
+            if (d <= 0)
                 continue;
             cv::Vec3f point = depthMap.at<cv::Vec3f>(y, x);
             objFile << "v " << point[0] << " " << point[1] << " " << point[2] << "\n";
@@ -281,6 +282,6 @@ int main(int argc, const char* argv[])
         }
     }
     objFile.close();
-    
+
     return 0;
 }
